5bb4f7b9b75100905a8ecccf78f24ab7040ba3c6
YevheniiKostrych <YevheniiKostrych@users.noreply.github.com>
2024-06-18 11:14:11 +0200

    4

--- src/main/java/Task1/ValidatorTest.java
+++ src/main/java/Task1/ValidatorTest.java
@@ -1,9 +1,6 @@
 package Task1;
-
-
 import java.util.ArrayList;
 import java.util.List;
-
 class ValidatorTest {
     public static void main(String[] args) {
         ValidatorOfList validatorOfList = new ValidatorOfList();
