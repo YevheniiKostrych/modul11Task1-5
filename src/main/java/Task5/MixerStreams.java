a753a4e86241baf18741755c22e89d1f4e5e9ecd
YevheniiKostrych <YevheniiKostrych@users.noreply.github.com>
2024-06-19 15:57:30 +0200

    6

--- src/main/java/Task5/MixerStreams.java
+++ src/main/java/Task5/MixerStreams.java
@@ -8,32 +8,13 @@ import java.util.stream.StreamSupport;
 
 public class MixerStreams {
     public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
-        Iterator<T> iterator1 = first.iterator();
-        Iterator<T> iterator2 = second.iterator();
+        List<T> firstList = first.collect(Collectors.toList());
+        List<T> secondList = second.collect(Collectors.toList());
+        int size = Math.min(firstList.size(), secondList.size());
 
-        Iterator<T> zippedIterator = new Iterator<T>() {
-            private boolean alternate = true;
+        return IntStream.range(0, size)
+                .boxed()
+                .flatMap(i -> Stream.of(firstList.get(i), secondList.get(i)));
 
-            public boolean hasNext() {
-                return iterator1.hasNext() && iterator2.hasNext();
-            }
-
-            public T next() {
-                if (!hasNext()) {
-                    throw new java.util.NoSuchElementException();
-                }
-                T nextElement;
-                if (alternate) {
-                    nextElement = iterator1.next();
-                } else {
-                    nextElement = iterator2.next();
-                }
-                alternate = !alternate;
-                return nextElement;
-            }
-        };
-
-        Spliterator<T> spliterator = Spliterators.spliteratorUnknownSize(zippedIterator, Spliterator.ORDERED);
-        return StreamSupport.stream(spliterator, false);
     }
 }
